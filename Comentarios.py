101b46f0404c4db870c72c4a0d8b8a1621e4f150
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-01-25 23:19:45 -0600

    Ejercicios Basicos Python.

---
+++ Comentarios.py
@@ -0,0 +1,13 @@
+#Los comentarios en Python están escritos con un carácter especial, ¿cuál? #
+
+# Esto es un comentario.
+
+# Use una cadena de varias líneas para hacer un comentario de varias líneas:
+'''
+
+Este es un comentario escrito
+en diferentes
+lineas, se pueden usar tantas como quieras.
+'''
+
+
---
+++ variables.py
@@ -0,0 +1,7 @@
+# Cree una variable llamada Nombre_del_Carro y asígnele el valor Volvo.
+
+Nombre_del_Carro = "Volvo"
+
+# Cree una variable llamada x y asígnele el valor 50.
+
+x = 50
