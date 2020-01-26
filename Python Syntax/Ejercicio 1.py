08820ea5894c7709b770c0896200ea33107abd5f
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-01-26 00:44:03 -0600

    Comentarios con "#".

---
+++ Python Syntax/Ejercicio 1.py
@@ -0,0 +1,3 @@
+# Inserte la parte faltante del código a continuación para generar "Hola mundo".
+
+print("Hola Mundo")
---
+++ Python Syntax/Ejercicio 2.py
@@ -0,0 +1,5 @@
+'''A este ejemplo le falta una sangrías para ser correcto.
+Inserte la sangría que falta para corregir el código:'''
+if 5 > 2:
+    print("Cinco es mayor a dos!")
+# para que se imprima el texto es necesario que haya una sangria si se esta utilizando if.
