cd543d6fc483ababbff8d81a9214be73210f56c3
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-01-26 00:45:53 -0600

    Sangria.

--- Python Syntax/Ejercicio 2.py
+++ Python Syntax/Ejercicio 2.py
@@ -2,4 +2,5 @@
 Inserte la sangría que falta para corregir el código:'''
 if 5 > 2:
     print("Cinco es mayor a dos!")
+
 # para que se imprima el texto es necesario que haya una sangria si se esta utilizando if.
