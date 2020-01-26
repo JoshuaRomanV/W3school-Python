410f8ebb2d83d26205a55eddd0796db0079f76dd
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-01-26 00:39:20 -0600

    Como usar comentarios en Python.

---
+++ Python Comments/Ejercicio 3.py
@@ -0,0 +1,5 @@
+#Los comentarios en Python están escritos con un carácter especial, ¿cuál? '#'
+
+# Esto es un comentario.
+
+
---
+++ Python Comments/Ejercicio 4.py
@@ -0,0 +1,7 @@
+#Use una cadena de varias líneas para hacer un comentario de varias líneas:
+
+'''
+Este es un comentario
+escrito en
+más que solo una línea
+'''
