76835981fc6a84f69808690dea8fde6752d37ea4
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-01-28 23:08:23 -0600

    valor en lista.

---
+++ Python Operators/Ejercicio 37.py
@@ -0,0 +1,6 @@
+# Utilice el operador de membresía correcto para verificar si "manzana" está presente en el objeto de frutas.
+
+frutas = ["manzana", "platano"]
+if "manzana" in frutas:
+  print("si, manzana es una fruta!")
+# Revisar si un valor o cadena esta en una lista.
