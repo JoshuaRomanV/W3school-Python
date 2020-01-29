798e77147b68e06c52dd37f410798ecb70dc4d39
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-01-28 23:08:50 -0600

    Valores diferentes.

---
+++ Python Operators/Ejercicio 38.py
@@ -0,0 +1,4 @@
+# Utilice el operador de comparación correcto para verificar si 5 no es igual a 10.
+
+if 5 !=10:
+  print("5 y 10 no son iguales.")
---
+++ Python Operators/Ejercicio 39.py
@@ -0,0 +1,4 @@
+# Utilice el operador lógico correcto para verificar si al menos una de las dos declaraciones es Verdadero.
+
+if 5 == 10 or 4 == 4:
+  print("Almenos una de las declaraciones es verdadera.")
