08df07585de1d35a2bd64e5423b620faf98f415d
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-02-01 02:36:06 -0600

    Agregar elementos al set con el metodo update.

---
+++ Python Sets/Ejercicio 54.py
@@ -0,0 +1,5 @@
+# Use el método correcto para agregar varios elementos (more_fruits) al conjunto de frutas.
+
+frutas = {"manzana", "plátano", "cereza"}
+more_fruits = ["naranja", "mango", "uvas"]
+frutas.update(more_fruits)
