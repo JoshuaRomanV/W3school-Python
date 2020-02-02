5feedc3639eae47f3f8ba7d4e2e49dbb4325fc32
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-02-01 21:12:59 -0600

    Agregar clave a un diccionario.

---
+++ Python Dictionaries/Ejercicio 59.py
@@ -0,0 +1,8 @@
+# Agregue el par clave / valor "color": "rojo" al diccionario del automóvil.
+
+coche = {
+    "marca": "Ford",
+    "modelo": "Mustang",
+    "año": 1964
+}
+coche["color"] = "rojo"
