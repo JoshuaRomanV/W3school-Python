47dfeea0330a8f091940958d8a5201cb9f8fbd6b
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-02-07 23:39:53 -0600

    Funcion range para recorrer la lista.

---
+++ Python For Loops/Ejercicio 77.py
@@ -0,0 +1,4 @@
+# Use la función de rango para recorrer un conjunto de códigos 6 veces.
+
+for x in range(6):
+    print(x)
---
+++ Python For Loops/Ejercicio 78.py
@@ -0,0 +1,7 @@
+# Salga del bucle cuando x es "banana".
+
+frutas = ["manzana", "plátano", "cereza"]
+for x in frutas:
+    if x == "banana":
+        break
+    print(x)
