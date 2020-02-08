167c210fa054ace42f02b76a23165adcf128d853
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-02-08 00:56:00 -0600

    Definir el numero de palabras clave de la función.

---
+++ Python Functions/Ejercicio 84.py
@@ -0,0 +1,4 @@
+# Si no conoce el número de argumentos de palabras clave que se pasarán a su función, hay un prefijo que puede agregar en la definición de la función, ¿qué prefijo?
+
+def my_function (**niño):
+    print ("Su apellido es" + niño ["lname"])
