965664399e9236f5b090892b7b639e8599d036a8
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-02-08 01:42:08 -0600

    Usar dir para imprimir todas las variables y nombres de un módulo.

---
+++ Python Modules/Ejercicio 94.py
@@ -0,0 +1,5 @@
+# ¿Cuál es la sintaxis correcta para imprimir todas las variables y nombres de funciones del módulo "mymodule"?
+
+
+import mymódulo
+print(dir(mymódulo))
---
+++ Python Modules/Ejercicio 95.py
@@ -0,0 +1,3 @@
+# ¿Cuál es la sintaxis correcta de importar solo el diccionario person1 del módulo "mymodule"?
+
+for mymodule import person1
