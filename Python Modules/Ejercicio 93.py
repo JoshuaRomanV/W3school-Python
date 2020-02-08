6f0e2c7d79c58b140ed599afe9250bd403ae501d
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-02-08 01:41:37 -0600

    Usar el alias de un módulo.

---
+++ Python Modules/Ejercicio 93.py
@@ -0,0 +1,6 @@
+# Si desea hacer referencia a un módulo utilizando un nombre diferente, puede crear un alias.
+
+# ¿Cuál es la sintaxis correcta para crear un alias para un módulo?
+
+
+import mymodule as mx
