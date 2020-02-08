42f83a4c8f4dbb94a93931437a7b0fd54dce84df
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-02-08 00:54:51 -0600

    Definir argumentos de la función.

---
+++ Python Functions/Ejercicio 83.py
@@ -0,0 +1,4 @@
+#Si no sabe la cantidad de argumentos que se pasarán a su función, hay un prefijo que puede agregar en la definición de la función, ¿qué prefijo?
+
+def my_function (*niños):
+    print ("El niño más pequeño es" + niños [2])
