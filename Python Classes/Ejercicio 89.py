418b385ce95f13993eac328b6db4e1ef1e284edf
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-02-08 01:26:30 -0600

    Utilizar init en un tipo de clase.

---
+++ Python Classes/Ejercicio 89.py
@@ -0,0 +1,6 @@
+# ¿Cuál es la sintaxis correcta para asignar una función "init" a una clase?
+
+class persona :
+    def __init__(auto, nombre, edad):
+        auto.nombre = nombre
+        auto.edad = edad
